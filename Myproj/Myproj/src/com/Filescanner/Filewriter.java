cbc070ac822b6c54d7c133c0a31e8682b54eb6b4
vigneshvasudevan <vigneshkutta@gmail.com>
2016-12-26 00:51:17 +0530

    Resubmit(2)

--- Myproj/Myproj/src/com/Filescanner/Filewriter.java
+++
@@ -1,8 +0,0 @@
-package com.Filescanner;
-
-import com.Filescanner.Node.Systemprops;
-
-public interface Filewriter {
-	void WriteToFile() ;
-	void Search(Tree tree,Systemprops key,String value);
-}
